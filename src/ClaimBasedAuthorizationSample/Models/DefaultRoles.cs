9ddb7c3c22934654e43e22cc647050a7e7597865
maaaruf <marufbdonline@gmail.com>
2022-09-13 00:33:45 +0600

    Added Seed method for create default roles

---
+++ src/ClaimBasedAuthorizationSample/Models/DefaultRoles.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace ClaimBasedAuthorizationSample.Models
+{
+    public static class DefaultRoles
+    {
+        public static async Task SeedAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
+        }
+    }
+}
