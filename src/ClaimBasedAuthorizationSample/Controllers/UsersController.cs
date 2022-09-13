e6c238de78203f22629ea23214d60851f3f1e5ee
maaaruf <marufbdonline@gmail.com>
2022-09-13 23:39:30 +0600

    Added Roles and Users Controller

---
+++ src/ClaimBasedAuthorizationSample/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClaimBasedAuthorizationSample.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
