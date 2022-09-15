119ab5ae8e4be038dd45bd79b33a834b09db3b40
maaaruf <marufbdonline@gmail.com>
2022-09-15 22:55:15 +0600

    Added ClaimsHelper and PermissionController

---
+++ src/ClaimBasedAuthorizationSample/Claims/ClaimsHelper.cs
@@ -0,0 +1,33 @@
+﻿using ClaimBasedAuthorizationSample.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace ClaimBasedAuthorizationSample.Claims
+{
+    public static class ClaimsHelper
+    {
+        public static void GetPermissions(this List<RoleClaimsViewModel> allPermissions, Type policy, string roleId)
+        {
+            FieldInfo[] fields = policy.GetFields(BindingFlags.Static | BindingFlags.Public);
+
+            foreach (FieldInfo fi in fields)
+            {
+                allPermissions.Add(new RoleClaimsViewModel { Value = fi.GetValue(null).ToString(), Type = "Permissions" });
+            }
+        }
+
+        public static async Task AddPermissionClaim(this RoleManager<IdentityRole> roleManager, IdentityRole role, string permission)
+        {
+            var allClaims = await roleManager.GetClaimsAsync(role);
+            if (!allClaims.Any(a => a.Type == "Permission" && a.Value == permission))
+            {
+                await roleManager.AddClaimAsync(role, new Claim("Permission", permission));
+            }
+        }
+    }
+}
