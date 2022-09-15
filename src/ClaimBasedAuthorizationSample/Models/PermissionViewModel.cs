ab1bcc5024861265cb5491666f8bbb9fc2f0e859
maaaruf <marufbdonline@gmail.com>
2022-09-15 22:43:15 +0600

    Added permission view model

---
+++ src/ClaimBasedAuthorizationSample/Models/PermissionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ClaimBasedAuthorizationSample.Models
+{
+    public class PermissionViewModel
+    {
+        public string RoleId { get; set; }
+        public IList<RoleClaimsViewModel> RoleClaims { get; set; }
+    }
+    public class RoleClaimsViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
